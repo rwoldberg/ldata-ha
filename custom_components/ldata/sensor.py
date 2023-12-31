34e1f46599a689c185e5656cbb12856d007aac8a
Robert Woldberg <robert@woldberg.com>
2023-12-31 13:01:55 -0700

    Add panel total daily energy

--- custom_components/ldata/sensor.py
+++ custom_components/ldata/sensor.py
@@ -89,7 +89,7 @@ async def async_setup_entry(
 
     for breaker_id in entry.data["breakers"]:
         breaker_data = entry.data["breakers"][breaker_id]
-        usage_sensor = LDATADailyUsageSensor(entry, breaker_data)
+        usage_sensor = LDATADailyUsageSensor(entry, breaker_data, False)
         async_add_entities([usage_sensor])
         power_sensor = LDATAOutputSensor(entry, breaker_data, SENSOR_TYPES[0])
         async_add_entities([power_sensor])
@@ -108,6 +108,8 @@ async def async_setup_entry(
         entity_data["hardware"] = "LDATA"
         entity_data["firmware"] = panel["firmware"]
         entity_data["poles"] = 2
+        usage_sensor = LDATADailyUsageSensor(entry, entity_data, True)
+        async_add_entities([usage_sensor])
         total_sensor = LDATATotalUsageSensor(
             entry, entity_data, SENSOR_TYPES[0], average=False, which_leg="both"
         )
@@ -150,7 +152,7 @@ class LDATADailyUsageSensor(LDATAEntity, RestoreSensor):
     _attr_device_class = SensorDeviceClass.ENERGY
     _attr_native_unit_of_measurement = UnitOfEnergy.KILO_WATT_HOUR
 
-    def __init__(self, coordinator: LDATAUpdateCoordinator, data) -> None:
+    def __init__(self, coordinator: LDATAUpdateCoordinator, data, panelTotal) -> None:
         """Init sensor."""
         super().__init__(data=data, coordinator=coordinator)
         self.breaker_data = data
@@ -158,6 +160,7 @@ class LDATADailyUsageSensor(LDATAEntity, RestoreSensor):
         self.last_update_time = 0.0
         self.previous_value = 0.0
         self.last_update_date = dt_util.now()
+        self.panel_total = panelTotal
 
     async def async_added_to_hass(self) -> None:
         """Handle entity which will be added."""
@@ -213,9 +216,20 @@ class LDATADailyUsageSensor(LDATAEntity, RestoreSensor):
     def _state_update(self):
         """Call when the coordinator has an update."""
         try:
-            if new_data := self.coordinator.data["breakers"][self.breaker_data["id"]]:
-                current_value = new_data["power"]
-
+            have_values = False
+            new_data = None
+            if self.panel_total is True:
+                current_value = 0
+                for breaker_id in self.coordinator.data["breakers"]:
+                    breaker_data = self.coordinator.data["breakers"][breaker_id]
+                    if breaker_data is not None:
+                        current_value += breaker_data["power"]
+                        have_values = True
+            else:
+                new_data = self.coordinator.data["breakers"][self.breaker_data["id"]]
+            if ((self.panel_total is True) and (have_values is True)) or (new_data is not None):
+                if new_data is not None:
+                    current_value = new_data["power"]
                 # Save the current date and time
                 current_time = time.time()
                 current_date = dt_util.now()
