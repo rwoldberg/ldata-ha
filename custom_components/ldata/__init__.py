d4bb323533c42691472400e11eb492e58b845e60
Robert Woldberg <robert@woldberg.com>
2024-07-06 12:48:23 -0600

    Update init.

--- custom_components/ldata/__init__.py
+++ custom_components/ldata/__init__.py
@@ -59,10 +59,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
