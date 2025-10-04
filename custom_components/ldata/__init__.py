dfd33e99c62b805808e5bef918c46ac2b9c898fe
MrToast99 <31112122+MrToast99@users.noreply.github.com>
2025-10-03 21:33:35 -0600

    Update __init__.py
    support for new loggin menu

--- custom_components/ldata/__init__.py
+++ custom_components/ldata/__init__.py
@@ -1,86 +1,47 @@
-"""The Leviton LDATA integration."""
-
+"""The LDATA integration."""
 from __future__ import annotations
 
-import asyncio
-import logging
-
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.helpers.typing import ConfigType
 
-from .const import (
-    DOMAIN,
-    LOGGER_NAME,
-    READ_ONLY,
-    READ_ONLY_DEFAULT,
-    THREE_PHASE,
-    THREE_PHASE_DEFAULT,
-    UPDATE_INTERVAL,
-    UPDATE_INTERVAL_DEFAULT,
-    UPDATE_INTERVAL_MIN,
-)
+from .const import DOMAIN
 from .coordinator import LDATAUpdateCoordinator
 
-PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.SENSOR, Platform.SWITCH]
-
-_LOGGER = logging.getLogger(LOGGER_NAME)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the LDATA component."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
-
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Leviton LDATA from a config entry."""
+    """Set up LDATA from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
-    user = entry.data[CONF_USERNAME]
-    password = entry.data[CONF_PASSWORD]
-    update_interval = entry.options.get(UPDATE_INTERVAL, UPDATE_INTERVAL_DEFAULT)
-    three_phase = entry.options.get(
-        THREE_PHASE, entry.data.get(THREE_PHASE, THREE_PHASE_DEFAULT)
+    
+    coordinator = LDATAUpdateCoordinator(
+        hass,
+        entry.data["email"],
+        entry.data["password"],
+        30, # Update interval in seconds
+        entry,
     )
-    read_only = entry.options.get(
-        READ_ONLY, entry.data.get(READ_ONLY, READ_ONLY_DEFAULT)
-    )
 
-    # Don't update more that every 10 seconds.
-    update_interval = max(update_interval, UPDATE_INTERVAL_MIN)
-
-    _LOGGER.debug("LDATA update interval: %d", update_interval)
-    _LOGGER.debug("LDATA three phase: %d", three_phase)
-    _LOGGER.debug("LDATA read only: %d", read_only)
-
-    coordinator = LDATAUpdateCoordinator(hass, user, password, update_interval, entry)
-
-    await coordinator.async_refresh()  # Get initial data
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
+    await coordinator.async_config_entry_first_refresh()
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    return True
+    # Add a listener to reload the integration when options are changed
+    entry.add_update_listener(async_reload_entry)
 
+    return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload config entry."""
+    await async_unload_entry(hass, entry)
+    await async_setup_entry(hass, entry)
